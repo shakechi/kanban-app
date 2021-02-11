522eedeca50dfb2013e326c5acd2543ce72e1393
shakechi <sctefh.dev@gmail.com>
2021-02-11 15:04:10 +0900

    update route

---
+++ src/router/rotes.js
@@ -0,0 +1,23 @@
+import KbnBoardView from '@/components/template/KbnBoardView.vue'
+import KbnLoginView from '@/components/template/KbnLoginView.Vue'
+import KbnTaskDetailModal from '@/components/template/KbnTaskDetailModal.vue'
+
+export default [{
+        path: '/',
+        component: KbnBoardView,
+        meta: {requiresAuth: true}
+    },
+    {
+        path: 'login',
+        component: KbnLoginView
+    },
+    {
+        path: '/tasks/:id',
+        component: KbnTaskDetailModal,
+        meta: { requiresAuth: true}
+    },
+    {
+        path: '*',
+        redirect: '/'
+    }
+]
