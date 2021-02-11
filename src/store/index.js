e490b4219b7e21c42fd02d2fae978bf9292c9e73
shakechi <sctefh.dev@gmail.com>
2021-02-11 11:24:27 +0900

    Vuex setting

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import actions from './actions'
+import getters from './getters'
+import mutations from './mutations'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    getters,
+    actions,
+    mutations,
+    // mutation以外でのstore変更を検知
+    // ただし、本番環境ではコストが高いので禁止
+    // https://vuex.vuejs.org/ja/guide/strict.html
+    strict: process.env.NODE_ENV !== 'production'
+})
